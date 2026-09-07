72162de59c7506492b3bdc7ec6d5849d470f7241
Umaima Zainab <umaimazainab429@gmail.com>
2026-09-07 19:16:49 +0500

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React, {useState} from 'react'
 
 const App = () => {
-  const [num, setnum] = useState("umaima zainab")
+  const [num, setnum] = useState("umaima")
+  const [use, setuse] = useState("30")
+  function changename(){
+     setuse("not 30")
+     setnum("not umaima")
+  }
   return (
     <div>
-      <h1>my name is {num} zainab!!</h1>
+      <h1>my name is {num} zainab!! <br /> my age is {use}</h1>
+      <button onClick={changename}>Click here!!</button>
     </div>
   )
 }
