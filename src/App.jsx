16a3ccef103852ae55b8f1e0e0f11033b4fb6ab0
Umaima Zainab <umaimazainab429@gmail.com>
2026-09-07 21:09:31 +0500

    Added loop practice questions

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,21 @@ const App = () => {
   function decrease() {
     setnum(num - 1)
   }
+  function jump(){
+    setnum(num + 5)
+  }
+  function del(){
+    setnum(num - num)
+  }
   return (
     <div>
       <h1>{num}</h1>
       <div className='btn'>
         <button onClick={increase}>Increase </button>
         <button onClick={decrease}>decrease</button>
+        <button onClick={jump}>increase by 5</button>
+        <button onClick={del}>delete</button>
+
       </div>  
       </div>
   )
